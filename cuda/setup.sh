e4349739ffcb2f5cd3dedf76fd755fd849573493
speed <speed@mail.ustc.edu.cn>
2025-03-21 17:01:51 +0800

    update

--- cuda/setup.sh
+++ cuda/setup.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-
 export DEBIAN_FRONTEND=noninteractive
-export MINICONDA_VERSION=latest
-export MINICONDA_PATH=/opt/miniconda
-export USERNAME=user
+export USERNAME=speed
 export USER_UID=1000
 
-sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
-sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+# 更换 apt 源为阿里云源
+sed -i 's@//.*archive.ubuntu.com@//mirrors.aliyun.com@g' /etc/apt/sources.list && \
+sed -i 's/security.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
 
 apt update
-apt upgrade -y
-apt install -y --no-install-recommends curl wget git sudo neovim tmux less ssh zsh zsh-syntax-highlighting
-rm -rf /var/lib/apt/lists/*
+apt install -y --no-install-recommends curl wget git sudo neovim tmux less ssh zsh 
 
-curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh
-bash miniconda.sh -b -p ${MINICONDA_PATH}
-rm miniconda.sh
+# 创建 speed 用户并赋予 sudo 权限
+useradd -m -s /usr/bin/zsh speed && \
+echo 'speed ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
+# 确保 zsh 配置文件存在，并修改 PS1
 userdel -rf ubuntu || true
 useradd -m ${USERNAME} --uid=${USER_UID}
 usermod -aG sudo ${USERNAME}
@@ -26,8 +23,24 @@ echo "${USERNAME} ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/${USERNAME}
 chown -R ${USERNAME}:${USERNAME} ${MINICONDA_PATH}
 chsh -s /bin/zsh ${USERNAME}
 
-sudo -u ${USERNAME} -i /bin/bash <<EOF
-sh -c "$(curl -fsSL https://install.ohmyz.sh)" "" --unattended
-git clone https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-EOF
+# 下载 Oh My Zsh
+git clone https://gitee.com/albpeed/ohmyzsh ~/.oh-my-zsh && \
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+# 安装 zsh 插件
+# RUN git clone https://gitee.com/albpeed/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && \
+#     git clone https://gitee.com/albpeed/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# # 配置 zsh 插件
+# # 配置 zshrc
+# RUN sed -i 's/plugins=(git)/plugins=(git extract zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc && \
+#     sed -i 's/ZSH_THEME=".*"/ZSH_THEME="ys"/' ~/.zshrc && \
+#     echo 'source $ZSH/oh-my-zsh.sh' >> ~/.zshrc && \
+#     echo 'source $ZSH_CUSTOM/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc && \
+#     echo 'source $ZSH_CUSTOM/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
+
+# # 修改 ys 主题，替换 %m 为 speed-zsh-docker
+# RUN sed -i 's/%m/speed-zsh-docker/g' ~/.oh-my-zsh/themes/ys.zsh-theme
+
+# # 设置容器启动时默认执行的命令
+# CMD ["zsh"]
