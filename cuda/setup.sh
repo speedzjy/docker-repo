ac25274e7e569d89b17b8364488016cc448df7c6
speed-labserver <speed@mail.ustc.edu.cn>
2025-03-22 22:54:52 +0800

    update

--- cuda/setup.sh
+++ cuda/setup.sh
@@ -26,8 +26,10 @@ git clone https://gitee.com/albpeed/ohmyzsh ~/.oh-my-zsh
 git clone https://gitee.com/albpeed/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://gitee.com/albpeed/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-mkdir -p /home/user/speed && cd /home/user/speed
-python3 -m venv rl_venv && source rl_venv/bin/activate
+mkdir -p /home/user/speed
+cd /home/user/speed
+python3 -m venv rl_venv
+source rl_venv/bin/activate
 mkdir -p /home/user/speed/rl_venv/ws
 
 pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
