52c6d56f47847a79b9db0b561fba4ddd00f2a98d
speed-labserver <speed@mail.ustc.edu.cn>
2025-03-22 23:15:23 +0800

    update

--- cuda/setup.sh
+++ cuda/setup.sh
@@ -39,24 +39,9 @@ mkdir -p /home/user/speed/rl_venv/ws
 # 安装 PyTorch
 pip install --upgrade pip
 pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+pip install tensorboard gym==0.19.0 colorama==0.4.6 numpy==1.26.4 pandas==2.2.3 setuptools
 
 # 生成 SSH 密钥
 mkdir -p ~/.ssh
 ssh-keygen -t rsa -b 4096 -C \"speed@docker.com\" -f ~/.ssh/id_rsa -N \"\" -q
 '"
-
-# # sudo -u ${USERNAME} -i /bin/bash <<EOF
-# git clone https://gitee.com/albpeed/ohmyzsh ~/.oh-my-zsh
-# git clone https://gitee.com/albpeed/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# git clone https://gitee.com/albpeed/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# mkdir -p /home/user/speed
-# cd /home/user/speed
-# python3 -m venv rl_venv
-# source rl_venv/bin/activate
-# mkdir -p /home/user/speed/rl_venv/ws
-
-# pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
-
-# ssh-keygen -t rsa -b 4096 -C "speed@docker.com" -f "$HOME/.ssh/id_rsa" -N "" -q
-# # EOF
