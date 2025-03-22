d8aac5826aab61dd5e393125dbe66c759c757395
speed <speed@mail.ustc.edu.cn>
2025-03-22 15:28:26 +0800

    raw docker

--- cuda/setup.sh
+++ cuda/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 export DEBIAN_FRONTEND=noninteractive
+export MINICONDA_VERSION=latest
+export MINICONDA_PATH=/opt/miniconda
 export USERNAME=user
 export USER_UID=1000
 
@@ -8,22 +11,23 @@ sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 
 apt update
 apt upgrade -y
-apt install -y --no-install-recommends curl wget git sudo neovim tmux less ssh zsh htop nvtop zsh-syntax-highlighting python3.10 python3.10-dev python3.10-venv python3.10-distutils python3-pip
+apt install -y --no-install-recommends curl wget git sudo neovim tmux less ssh zsh zsh-syntax-highlighting
 rm -rf /var/lib/apt/lists/*
+
+curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh
+bash miniconda.sh -b -p ${MINICONDA_PATH}
+rm miniconda.sh
 
 userdel -rf ubuntu || true
 useradd -m ${USERNAME} --uid=${USER_UID}
 usermod -aG sudo ${USERNAME}
-
-echo "${USERNAME}:password" | chpasswd
-echo "${USERNAME} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${USERNAME}
-chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}
+echo 'user:password' | chpasswd
+echo "${USERNAME} ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/${USERNAME}
+chown -R ${USERNAME}:${USERNAME} ${MINICONDA_PATH}
 chsh -s /bin/zsh ${USERNAME}
 
 sudo -u ${USERNAME} -i /bin/bash <<EOF
-git clone https://gitee.com/albpeed/ohmyzsh ~/.oh-my-zsh
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-git clone https://gitee.com/albpeed/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://gitee.com/albpeed/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sh -c "$(curl -fsSL https://install.ohmyz.sh)" "" --unattended
+git clone https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 EOF
-
