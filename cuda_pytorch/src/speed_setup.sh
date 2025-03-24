7e731a221e0aed70d8f5a4be33c7c62dcc28ef2a
speed-labserver <speed@mail.ustc.edu.cn>
2025-03-24 21:07:44 +0800

    update

--- cuda_pytorch/src/speed_setup.sh
+++ cuda_pytorch/src/speed_setup.sh
@@ -6,3 +6,6 @@ cp -r /data/cls1-srv2-pool/.ssh/* /home/user/.ssh/
 chmod 700 /home/user/.ssh
 chmod 600 /home/user/.ssh/id_rsa
 chmod 644 /home/user/.ssh/id_rsa.pub
+
+git config --global user.name "speed-docker"
+git config --global user.email "speed@docker.com"
